88c0454c1cd0d1a2e8595464ffdf7fd27b5a6513
bradford.foxworthhill <bradford.foxworthhill@atlanticbt.com>
2016-01-21 01:33:31 -0500

    added bower and gulp

--- GotNext.Client/gulpfile.js
+++ GotNext.Client/gulpfile.js
@@ -1,5 +1,29 @@
 ﻿var gulp = require('gulp');
+var inject = require('gulp-inject');
+var mainBowerFiles = require('main-bower-files');
+var debug = require('gulp-debug');
+
+var config = {
+    js: ['!./app/app.module.js',
+        '!./app/app.core.module.js',
+        './app/*module.js',
+        './app/*.js',
+        './app/**/*module.js',
+        './app/**/*.js']
+};
 
 gulp.task('hello-world', function() {
     console.log('Our Hello World');
+});
+
+gulp.task('app-scripts', function () {
+    return gulp.src('./index.html')
+        .pipe(inject(gulp.src(config.js, { read: false }), { name: 'scripts' }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('bower-scripts', function () {
+    return gulp.src('./index.html')
+        .pipe(inject(gulp.src(mainBowerFiles(), { read: false }, { relative: true }), { name: 'bower' }))
+        .pipe(gulp.dest('./'));
 });
